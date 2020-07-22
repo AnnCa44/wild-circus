af8c310c1b692bdba90e3a49253c1687b49563d4
AnnCa44 <ann.cariou@gmail.com>
2020-07-22 11:11:58 +0200

    create entity article

---
+++ src/main/java/com/wildcodeschool/wildCircus/entity/Article.java
@@ -0,0 +1,56 @@
+package com.wildcodeschool.wildCircus.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "article")
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String image;
+
+    @Column
+    private String text;
+
+    @Column
+    private String performance;
+
+    public Article() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getPerformance() {
+        return performance;
+    }
+
+    public void setPerformance(String performance) {
+        this.performance = performance;
+    }
+}
