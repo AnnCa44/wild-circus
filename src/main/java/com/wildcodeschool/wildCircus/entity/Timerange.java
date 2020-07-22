8de947235247534c45a24110ec4e10c7873625da
AnnCa44 <ann.cariou@gmail.com>
2020-07-22 11:04:38 +0200

    create entity timerange

--- src/main/java/com/wildcodeschool/wildCircus/entity/Timerange.java
+++ src/main/java/com/wildcodeschool/wildCircus/entity/Timerange.java
@@ -1,4 +1,34 @@
 package com.wildcodeschool.wildCircus.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "timerange")
 public class Timerange {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String label;
+
+    public Timerange() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
