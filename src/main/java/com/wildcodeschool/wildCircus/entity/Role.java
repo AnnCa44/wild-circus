331ddc7b93c2b08e2d2f4bc23684faed0fc33bf1
AnnCa44 <ann.cariou@gmail.com>
2020-07-22 12:03:21 +0200

    add role repository and entity

---
+++ src/main/java/com/wildcodeschool/wildCircus/entity/Role.java
@@ -0,0 +1,34 @@
+package com.wildcodeschool.wildCircus.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
