d5ca0f2097a35e0d0ed17eda30ba6ec890fcfe24
AnnCa44 <ann.cariou@gmail.com>
2020-07-23 22:27:32 +0200

    add section aboutUs fix css all pages

--- src/main/java/com/wildcodeschool/wildCircus/controller/MainController.java
+++ src/main/java/com/wildcodeschool/wildCircus/controller/MainController.java
@@ -3,11 +3,18 @@ package com.wildcodeschool.wildCircus.controller;
 import com.wildcodeschool.wildCircus.entity.Article;
 import com.wildcodeschool.wildCircus.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Controller
 public class MainController {
@@ -15,10 +22,13 @@ public class MainController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @GetMapping("/")
     public String index(Model out) {
 
-        out.addAttribute("articles", articleRepository.findAll().subList(0,3));
+        out.addAttribute("articles", articleRepository.findAll());
         return "index";
     }
 
@@ -40,5 +50,33 @@ public class MainController {
     public String priceContact() {
 
         return "price-contact";
+    }
+
+    @GetMapping("/email")
+    public String email(@RequestParam String name,
+                        @RequestParam String mail,
+                        @RequestParam String message) {
+
+        try {
+            sendEmail(name, mail, message);
+        } catch (MailException | MessagingException exception) {
+            return exception.getMessage();
+        }
+        return "price-contact";
+    }
+
+    void sendEmail(String name, String mail, String message) throws MailException, MessagingException {
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, false);
+
+        helper.setTo(name);
+
+        helper.setSubject(mail);
+        helper.setText("<h1>" + message + "</h1><br><a href=\"#lol\">Se désabonner</a>", true);
+
+        javaMailSender.send(msg);
     }
 }
